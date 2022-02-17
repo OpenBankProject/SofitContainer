62eb7678c8e43833106b11434a2c640c2c41dd5f
Reena_Aheer <ranireena68@gmail.com>
2022-02-17 07:46:21 +0100

    Code Clean

--- www/js/index.js
+++ www/js/index.js
@@ -1,69 +1,87 @@
     /**
-            Open Bank Project Sofit Container
-            Copyright (C) 2021-2022, TESOBE GmbH.
+                Open Bank Project Sofit Container
+                Copyright (C) 2021-2022, TESOBE GmbH.
 
-            This program is free software: you can redistribute it and/or modify
-            it under the terms of the GNU Affero General Public License as published by
-            the Free Software Foundation, either version 3 of the License, or
-            (at your option) any later version.
+                This program is free software: you can redistribute it and/or modify
+                it under the terms of the GNU Affero General Public License as published by
+                the Free Software Foundation, either version 3 of the License, or
+                (at your option) any later version.
 
-            This program is distributed in the hope that it will be useful,
-            but WITHOUT ANY WARRANTY; without even the implied warranty of
-            MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-            GNU Affero General Public License for more details.
+                This program is distributed in the hope that it will be useful,
+                but WITHOUT ANY WARRANTY; without even the implied warranty of
+                MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+                GNU Affero General Public License for more details.
 
-            You should have received a copy of the GNU Affero General Public License
-            along with this program.  If not, see <http://www.gnu.org/licenses/>.
+                You should have received a copy of the GNU Affero General Public License
+                along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-            Email: contact@tesobe.com
-            TESOBE GmbH.
-            Osloer Strasse 16/17
-            Berlin 13359, Germany
+                Email: contact@tesobe.com
+                TESOBE GmbH.
+                Osloer Strasse 16/17
+                Berlin 13359, Germany
 
-            http://www.tesobe.com
-            */
+                http://www.tesobe.com
+                */
 
     /** Wait for the deviceready event before using any of Cordova's device APIs. The Deviceready event fires when Cordova is fully loaded*/
     document.addEventListener('deviceready', onDeviceReady, false)
     const consumer_key = 'xznnnce3pn3v5lddyayuiphc1cvm25fykx1y034l'
     const SOFIT_HOST = 'https://includimi-sofit.tesobe.com'
     const OBP_API_HOST = 'https://includimi.tesobe.com'
-    let correlated_user_id = ''
     //Token will expire within 4 Weeks
     let token_life = 27 * 24 * 60 * 60 * 1000
 
     /** For each event handler registered after the deviceready event fires has its callback function called immediately. */
     function onDeviceReady() {
-      getDirectLoginTokenWSE()
-      openSofit()
+      let name_user = window.localStorage.getItem('username') // variable name_user is used to store the item user name property, so that can properly user the data.
+      let user_password = window.localStorage.getItem('password')
+      let correlated_user_id = window.localStorage.getItem('correlated_user_id')
+      if (directLoginTokenExistsLocally() && directLoginTokenIsFresh() && localDirectLoginTokenIsValid())
+      {
+      } else if (
+        correlatedUserExistsLocally(name_user, user_password, correlated_user_id)
+      ) {
+        createAndStoreNewToken(name_user, user_password)
+      } else if (!correlatedUserExistsLocally(name_user, user_password, correlated_user_id))
+        {
+        // if user_id does not exist in local storage that means user has not register or new user then call create new user.
+            createNewUser()
+         } else {
+        return 'error'
+      }
+      openSofit(correlated_user_id)
     }
 
-    /**  - This function retrieves the direct login token from local storage, as well as checks whether the token is valid or not?
-                   - As well as, get a date_token_generated from local storage and try to compare the expiry date of the token, if it will expire soon, then will generate token before expire.*/
-    function getDirectLoginTokenWSE() {
-      /** param {[string]} correlated_user_id: This id will get from the local storage */
-      let date_token_generated = window.localStorage.getItem(' date_token_generated')
+    function directLoginTokenIsFresh() {
+      let date_token_generated = window.localStorage.getItem('date_token_generated')
       let tokenDuration = date_token_generated + token_life
       let today_date = new Date().getTime()
-      if (localDirectLoginTokenIsValid() && tokenDuration < today_date) {
-        return createNewDirectLoginToken()
+      return tokenDuration < today_date //it is boolen value:: if token duration is grater then token date
+    }
+
+    function correlatedUserExistsLocally(username, password, correlated_user_id) {
+      if (username && password && correlated_user_id) {
+        return true
       } else {
         return false
       }
-    }
+    } //return undefied, if correlated_user_id not found in local storage then return undefined
 
     /** This function checks if the token is valid or not.
-                        GUARD: 1.  Two options are available for checking the token validation.
-                                  -The token should be present or expired in the local storage.
-                                  -Generation of the token by an invalid user.
-                               2. If the token is valid, call the API to get the current login user. */
+                            GUARD: 1.  Two options are available for checking the token validation.
+                                      -The token should be present or expired in the local storage.
+                                      -Generation of the token by an invalid user.
+                                   2. If the token is valid, call the API to get the current login user. */
     function localDirectLoginTokenIsValid() {
-      if (directLoginTokenIsExistIsLocallyWSE()) {
+      if (directLoginTokenExistsLocally()) {
         cordova.plugin.http.setDataSerializer('json')
         //Set the Header parameter for the Post request
         cordova.plugin.http.setHeader(
           'Authorization',
-          `DirectLogin token = "${window.localStorage.getItem('direct_login_token', )}"`,)
+          `DirectLogin token = "${window.localStorage.getItem(
+            'direct_login_token',
+          )}"`,
+        )
         cordova.plugin.http.setHeader('Content-Type', 'application/json ')
         //Post request create, leave body and header section empty because defined above
         cordova.plugin.http.get(
@@ -72,7 +90,7 @@
           {},
           function (response) {
             return true
-          }
+          },
         )
       } else {
         return false
@@ -80,7 +98,7 @@
     }
 
     /** This function checks whether the token is exists in local local storage or not. If the token is present, the endpoint is called and the current login user is returned. */
-    function directLoginTokenIsExistIsLocallyWSE() {
+    function directLoginTokenExistsLocally() {
       if (window.localStorage['direct_login_token']) {
         return true
       } else {
@@ -89,21 +107,22 @@
     }
 
     /** Call the function to create a new token.
-        1. call the API and return the direct login token. */
+            1. call the API and return the direct login token. */
     function createNewDirectLoginToken() {
-      var user = createNewUser()
+      createNewUser()
     }
 
     /** This function creates a new user.
      * @param {[object]} json Set the http request type json. */
-    function createNewUser() {
-      return new Promise((resolve, reject) => {
+    async function createNewUser() {
+      const res = await new Promise((resolve, reject) => {
         cordova.plugin.http.setDataSerializer('json')
         // get unique id to create user : uuid
         const uuid_string = device.uuid
         // These lines of code will generate a dynamic password with long string.
         var randomLongStringPassword = ''
-        var characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+        var characters =
+          'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
         var charactersLength = characters.length
         for (var i = 0; i < 20; i++) {
           randomLongStringPassword += characters.charAt(
@@ -130,17 +149,24 @@
             if (response.status == 201) {
               let user_data = JSON.parse(response.body)
               let time = new Date().getTime()
-              correlated_user_id = user_data.user_id
+              let correlated_user_id = user_data.user_id
+              let username = user_data.username
+              let password = user_data.password
               window.localStorage.setItem('correlated_user_id', correlated_user_id)
-              window.localStorage.setItem(' date_token_generated', time)
+              window.localStorage.setItem('username', username)
+              window.localStorage.setItem('password', password)
+              window.localStorage.setItem('date_token_generated', time)
             }
             resolve(createUserOptions.data)
           },
         )
       })
+      if (res) {
+        return true
+      }
     }
 
-    /** The token is stroed in local memory after generation. */
+    /** The token is stored in local memory after generation. */
     function storeNewDirectLoginTokenWSE(token) {
       var storage = window.localStorage
       storage.setItem('direct_login_token', token)
@@ -148,12 +174,14 @@
     }
 
     /** This is used for the creation of a new token. */
-    function generatedToken(username, password) {
+    function createAndStoreNewToken(username, password) {
       cordova.plugin.http.setDataSerializer('json')
       //Set the header parameter for the post request.
       //update value in direct login
       cordova.plugin.http.setHeader(
-        'Authorization', 'DirectLogin username= + username , password= + password, consumer_key= + consumer_key')
+        'Authorization',
+        `DirectLogin username=${username} , password= ${password}, consumer_key= + consumer_key`,
+      )
       cordova.plugin.http.setHeader('Content-Type', 'application/json ')
       //Create the post request, leave the body and header section empty as it was defined above.
       cordova.plugin.http.post(
@@ -169,6 +197,10 @@
     }
 
     /** This function is used to open the Sofit App with user ID. */
-    function openSofit() {
-      window.open = cordova.InAppBrowser.open(`${SOFIT_HOST}?correlated_user_id = ${correlated_user_id}`, '_blank', 'location=no')
+    function openSofit(user_id) {
+      window.open = cordova.InAppBrowser.open(
+        `${SOFIT_HOST}?correlated_user_id = ${user_id}`,
+        '_blank',
+        'location=no',
+      )
     }
